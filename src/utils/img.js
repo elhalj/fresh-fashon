c5575e4e1e54c16bb8ce30ade70e525a3f868366
elhalj <wilsonikedakoffi7@gmail.com>
2024-12-28 17:35:41 +0000

    media

--- src/utils/img.js
+++ src/utils/img.js
@@ -5,50 +5,50 @@ const Homeproducts = [
         id:1,
         nom:"chausssure",
         type:"top",
-        image:"image/chaussures.jpg"
+        image:"https://media.istockphoto.com/id/1279108197/fr/photo/vari%C3%A9t%C3%A9-de-chaussures-confortables-de-mode-de-femmes-de-toutes-saisons-sur-un-fond-l%C3%A9ger-vue.jpg?s=612x612&w=0&k=20&c=UZqf007bciqUY_KI1kC1v3yGwrM_ZvWZvla0moau7vQ="
     },
     {
         id:2,
         nom:"pentalon",
         type:"top",
-        image:"image/pentl1.jpeg"
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6tEBSVesWZTub3sSgj4xuVcAu9BMFQC5HGA&s"
     },
     {
         id:3,
         nom:"tee-shirt",
         type:"top",
-        image:"image/teshirt.avif"
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8CDtmGvcBCW5nor0OTBoUepVhtu82U7NnrA&s"
     },
     {
         id:4,
         nom:"television",
         type:"new",
-        image:"image/tele.jpg"
+        image:"https://media.istockphoto.com/id/1395191574/fr/photo/noir-led-tv-%C3%A9cran-de-t%C3%A9l%C3%A9vision-vierge-isol%C3%A9.jpg?s=612x612&w=0&k=20&c=e0CTyOK3YOg9VVdc1bvW0yq7rBhPZ0LkIPGXDZFCfR4="
     },
     {
         id:5,
         nom:"casque",
         type:"feature",
-        image:"image/premium_photo-1679513691474-73102089c117.jpeg"
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqXH6ghQNWGvhfQMAtgHt21Up4BcQz4P2nMg&s"
     },
     {
         id:6,
         nom:"ordinateur",
         type:"top",
-        image:"image/ordiPortable.webp"
+        image:"https://im.qccdn.fr/node/decryptage-pc-de-bureau-ordinateur-portable-ou-tablette-quel-ordinateur-pour-tous-les-jours-2189/thumbnail_1000x600px-124338.jpg"
     },
     {
         id:7,
         nom:"jouets",
         type:"new",
-        image:"image/jouet.jpeg"
+        image:"https://img.20mn.fr/zpD-fHw4RGCTBBg61n05KA/1444x920_profitez-meilleures-promos-redoute-jouets-cadeaux-noel-enfants"
     },
     {
         id:8,
         nom:"portable",
         type:"new",
-        image:"image/port.webp"
-    }
+        image:"https://sodishop.bmperp.com/assets/uploads/produits/telephone-portable-smartphone-samsung-galaxy-a23-4-go-de-ram-64-go-de-stockage-ma0016-338199.jpg"
+    },
 ]
 
 export default Homeproducts;
